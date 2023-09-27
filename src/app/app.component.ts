fe9d6e021e724f0a8b5e1f70f9d46ea340084c95
isabelle-lemire-AEC <isabelle.lemire.02@edu.cegeptr.qc.ca>
2023-09-26 21:19:48 -0400

    Mon premier test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mon_premier_projet_test';
+  titrePrincipal = 'mon_premier_projet_test';
+  sousTitre = 'Je suis un sous-titre';
+  description = 'Je suis une description';
 }
